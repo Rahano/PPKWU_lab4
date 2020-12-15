a499ae91ad6d2be81384dd5c49a26f51bab56293
Rahano <wojciechmuras@wp.pl>
2020-12-15 14:48:08 +0100

    Filter data for creating vCard

--- src/main/java/ppkwu/lab4/VCardController.java
+++ src/main/java/ppkwu/lab4/VCardController.java
@@ -2,6 +2,8 @@ package ppkwu.lab4;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,12 @@ public class VCardController {
 
         Document document = Jsoup.connect(url).get();
         //System.out.println(document);
+        Elements elements = document.select("script");
+        for (Element element : elements) {
+            if (element.attr("type").equals("application/ld+json")) {
+                System.out.println(element);
+            }
+        }
 
     }
 
