8546557536cd5769969157dd40df8ad4ae71a31a
Rahano <wojciechmuras@wp.pl>
2020-12-15 14:27:15 +0100

    Add input parameter for getting specific service

--- src/main/java/ppkwu/lab4/VCardController.java
+++ src/main/java/ppkwu/lab4/VCardController.java
@@ -1,13 +1,14 @@
 package ppkwu.lab4;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VCardController {
 
     @RequestMapping(path = "/getVCards")
-    public void publishVCards(){
+    public void publishVCards(@RequestParam(value = "service") String service){
 
     }
 
