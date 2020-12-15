c57757e5e5225966de13440da72fd13b828005d4
Rahano <wojciechmuras@wp.pl>
2020-12-15 14:38:29 +0100

    Collect data from panoramafirm.pl using Jsoup

--- src/main/java/ppkwu/lab4/VCardController.java
+++ src/main/java/ppkwu/lab4/VCardController.java
@@ -1,17 +1,24 @@
 package ppkwu.lab4;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class VCardController {
 
     @RequestMapping(path = "/getVCards")
-    public void publishVCards(@RequestParam(value = "service") String service){
+    public void publishVCards(@RequestParam(value = "service") String service) throws IOException {
 
         String url = "https://panoramafirm.pl/szukaj?k=";
         url += service + "&l=";
+
+        Document document = Jsoup.connect(url).get();
+        //System.out.println(document);
 
     }
 
