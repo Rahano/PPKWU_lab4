d86366d2387b84b2537f06f13dfc2c5718e71683
Rahano <wojciechmuras@wp.pl>
2020-12-15 14:25:10 +0100

    Add mapping for getting vCards

--- src/main/java/ppkwu/lab4/VCardController.java
+++ src/main/java/ppkwu/lab4/VCardController.java
@@ -1,8 +1,14 @@
 package ppkwu.lab4;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VCardController {
+
+    @RequestMapping(path = "/getVCards")
+    public void publishVCards(){
+
+    }
 
 }
